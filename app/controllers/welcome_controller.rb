e83fab9a601777ab24da1806faa70879e4438c2e
Alexander <alexanderlongumolsen@gmail.com>
2022-04-11 20:05:17 +0200

    Fikset landingpage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    @newest = Video.order(created_at: :desc).limit(10)
   end
 
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,6 @@
 class Video < ApplicationRecord
   has_one :video_file
+
+  validates :title, presence: true
+  validates :video_file, presence: true
 end
